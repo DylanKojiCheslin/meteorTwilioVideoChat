cf23768e2d5cc77edbff3908a1126ccbef4d81bc
dylan <dylan.kojicheslin@gmail.com>
2017-10-15 04:27:28 -0600

    connect to room

--- client/templates/videoChat.js
+++ client/templates/videoChat.js
@@ -6,6 +6,11 @@ function attachTracksToDomElement(tracks, domElement) {
   });
 }
 
+function attachParticipantTracks(participant, domElement) {
+  var tracks = Array.from(participant.tracks.values());
+  attachTracks(tracks, domElement);
+}
+
 function muteTracks(tracks) {
   tracks.forEach(function(track) {
     if (track.kind == "audio") {
@@ -22,9 +27,40 @@ function unmuteTracks(tracks) {
   });
 }
 
+function roomJoined(room){
+  console.log('roomJoined');
+  console.log(room);
+}
+
+function createLocalTracks(template) {
+  return new Promise((resolve, reject) => {
+    const localMediaElement = document.getElementById("local-media");
+    Video.createLocalTracks().then(
+      function(tracks) {
+        template.localTracks = tracks;
+        attachTracksToDomElement( tracks, localMediaElement );
+        resolve(tracks);
+    }, function(error) {
+      console.error('Unable to access local media', error);
+      reject('Unable to access Camera and Microphone');
+    });
+  });
+}
+
 Template.videoChat.onCreated(function (){
    this.previewVariable = new ReactiveVar(null);
    this.localMuted = new ReactiveVar(null);
+   const self = this;
+   Meteor.call(
+     "requestVideoChatAccess", function(error, result){
+       if (error) {
+         console.error(error.reason)
+       }
+       if (result) {
+         self.accessData = result;
+       }
+     });
+
 });
 
 Template.videoChat.onRendered(function (){
@@ -41,20 +77,18 @@ Template.videoChat.helpers({
   }
 });
 
-
 Template.videoChat.events({
   "click #js-preview-camera": function(event, template){
     event.preventDefault();
-    var localMediaElement = document.getElementById("local-media");
     if( ! template.localTracks){
-      Video.createLocalTracks().then(function(tracks) {
-        template.localTracks = tracks;
-        attachTracksToDomElement( tracks, localMediaElement );
+      createLocalTracks(template).then(
+        function(value){
         template.previewVariable.set(true);
-      }, function(error) {
-        console.error('Unable to access local media', error);
-        log('Unable to access Camera and Microphone');
-      });
+      },
+        function(error){
+          console.error(error);
+        }
+      )
     }
   },
   "click #js-mute-local-media": function(event, template){
@@ -63,7 +97,6 @@ Template.videoChat.events({
     if( template.localTracks){
       muteTracks(template.localTracks);
       template.localMuted.set(true);
-      console.log(template);
     }
   },
   "click #js-unmute-local-media": function(event, template){
@@ -72,186 +105,23 @@ Template.videoChat.events({
     if( template.localTracks){
       unmuteTracks(template.localTracks);
       template.localMuted.set(false);
-      console.log(template);
     }
   },
+  "submit #js-join-video-chat-room": function(event, template){
+    event.preventDefault();
+    let theLocalTracks;
+    template.previewVariable.set(true);
+    const roomName = event.target.text.value;
+    let connectOptions = {
+      name: roomName,
+      logLevel: 'debug'
+    }
+    if ( template.localTracks ) {
+      connectOptions.tracks = template.localTracks;
+    }
+    Video.connect(template.accessData.token, connectOptions)
+    .then(roomJoined, function(error) {
+      console.error('Could not connect to Twilio: ' + error.message);
+    });
+  }
 });
-
-// // Preview LocalParticipant's Tracks.
-// document.getElementById('button-preview').onclick = function() {
-//   var localTracksPromise = previewTracks
-//     ? Promise.resolve(previewTracks)
-//     : Video.createLocalTracks();
-//
-//   localTracksPromise.then(function(tracks) {
-//     var previewContainer = document.getElementById('local-media');
-//     if (!previewContainer.querySelector('video')) {
-//       attachTracks(tracks, previewContainer);
-//     }
-//   }, function(error) {
-//     console.error('Unable to access local media', error);
-//     log('Unable to access Camera and Microphone');
-//   });
-// };
-
-
-// var activeRoom;
-// var previewTracks;
-// var identity;
-// var roomName;
-//
-// // Attach the Participant's Tracks to the DOM.
-// function attachParticipantTracks(participant, container) {
-//   var tracks = Array.from(participant.tracks.values());
-//   attachTracks(tracks, container);
-// }
-//
-// // Detach the Tracks from the DOM.
-// function detachTracks(tracks) {
-//   tracks.forEach(function(track) {
-//     track.detach().forEach(function(detachedElement) {
-//       detachedElement.remove();
-//     });
-//   });
-// }
-//
-// // Detach the Participant's Tracks from the DOM.
-// function detachParticipantTracks(participant) {
-//   var tracks = Array.from(participant.tracks.values());
-//   detachTracks(tracks);
-// }
-//
-// // When we are about to transition away from this page, disconnect
-// // from the room, if joined.
-// window.addEventListener('beforeunload', leaveRoomIfJoined);
-//
-// // Obtain a token from the server in order to connect to the Room.
-// $.getJSON('/token', function(data) {
-//   identity = data.identity;
-//   document.getElementById('room-controls').style.display = 'block';
-//
-//   // Bind button to join Room.
-//   document.getElementById('button-join').onclick = function() {
-//     roomName = document.getElementById('room-name').value;
-//     if (!roomName) {
-//       alert('Please enter a room name.');
-//       return;
-//     }
-//
-//     log("Joining room '" + roomName + "'...");
-//     var connectOptions = {
-//       name: roomName,
-//       logLevel: 'debug'
-//     };
-//
-//     if (previewTracks) {
-//       connectOptions.tracks = previewTracks;
-//     }
-//
-//     // Join the Room with the token from the server and the
-//     // LocalParticipant's Tracks.
-//     Video.connect(data.token, connectOptions).then(roomJoined, function(error) {
-//       log('Could not connect to Twilio: ' + error.message);
-//     });
-//   };
-//
-//   // Bind button to leave Room.
-//   document.getElementById('button-leave').onclick = function() {
-//     log('Leaving room...');
-//     activeRoom.disconnect();
-//   };
-// });
-//
-// // Successfully connected!
-// function roomJoined(room) {
-//   window.room = activeRoom = room;
-//
-//   log("Joined as '" + identity + "'");
-//   document.getElementById('button-join').style.display = 'none';
-//   document.getElementById('button-leave').style.display = 'inline';
-//
-//   // Attach LocalParticipant's Tracks, if not already attached.
-//   var previewContainer = document.getElementById('local-media');
-//   if (!previewContainer.querySelector('video')) {
-//     attachParticipantTracks(room.localParticipant, previewContainer);
-//   }
-//
-//   // Attach the Tracks of the Room's Participants.
-//   room.participants.forEach(function(participant) {
-//     log("Already in Room: '" + participant.identity + "'");
-//     var previewContainer = document.getElementById('remote-media');
-//     attachParticipantTracks(participant, previewContainer);
-//   });
-//
-//   // When a Participant joins the Room, log the event.
-//   room.on('participantConnected', function(participant) {
-//     log("Joining: '" + participant.identity + "'");
-//   });
-//
-//   // When a Participant adds a Track, attach it to the DOM.
-//   room.on('trackAdded', function(track, participant) {
-//     log(participant.identity + " added track: " + track.kind);
-//     var previewContainer = document.getElementById('remote-media');
-//     attachTracks([track], previewContainer);
-//   });
-//
-//   // When a Participant removes a Track, detach it from the DOM.
-//   room.on('trackRemoved', function(track, participant) {
-//     log(participant.identity + " removed track: " + track.kind);
-//     detachTracks([track]);
-//   });
-//
-//   // When a Participant leaves the Room, detach its Tracks.
-//   room.on('participantDisconnected', function(participant) {
-//     log("Participant '" + participant.identity + "' left the room");
-//     detachParticipantTracks(participant);
-//   });
-//
-//   // Once the LocalParticipant leaves the room, detach the Tracks
-//   // of all Participants, including that of the LocalParticipant.
-//   room.on('disconnected', function() {
-//     log('Left');
-//     if (previewTracks) {
-//       previewTracks.forEach(function(track) {
-//         track.stop();
-//       });
-//     }
-//     detachParticipantTracks(room.localParticipant);
-//     room.participants.forEach(detachParticipantTracks);
-//     activeRoom = null;
-//     document.getElementById('button-join').style.display = 'inline';
-//     document.getElementById('button-leave').style.display = 'none';
-//   });
-// }
-//
-// // Preview LocalParticipant's Tracks.
-// document.getElementById('button-preview').onclick = function() {
-//   var localTracksPromise = previewTracks
-//     ? Promise.resolve(previewTracks)
-//     : Video.createLocalTracks();
-//
-//   localTracksPromise.then(function(tracks) {
-//     window.previewTracks = previewTracks = tracks;
-//     var previewContainer = document.getElementById('local-media');
-//     if (!previewContainer.querySelector('video')) {
-//       attachTracks(tracks, previewContainer);
-//     }
-//   }, function(error) {
-//     console.error('Unable to access local media', error);
-//     log('Unable to access Camera and Microphone');
-//   });
-// };
-//
-// // Activity log.
-// function log(message) {
-//   var logDiv = document.getElementById('log');
-//   logDiv.innerHTML += '<p>&gt;&nbsp;' + message + '</p>';
-//   logDiv.scrollTop = logDiv.scrollHeight;
-// }
-//
-// // Leave Room.
-// function leaveRoomIfJoined() {
-//   if (activeRoom) {
-//     activeRoom.disconnect();
-//   }
-// }
--- server/requestVideoChatAccess.js
+++ server/requestVideoChatAccess.js
@@ -1,49 +1,22 @@
-import { AccessToken } from 'twilio.jwt.AccessToken';
+const AccessToken = require('twilio').jwt.AccessToken;
 const VideoGrant = AccessToken.VideoGrant;
 
 Meteor.methods({
-  requestVideoChatAccess:function(roomName){
-    check(roomName, String);
-    const token = new AccessToken(
-      Meteor.settings.private.twilio.accountSid,
-      Meteor.settings.private.twilio.apiKey,
-      Meteor.settings.private.twilio.apiKeySecret
-    );
-    token.identity = Random.id();
-    const grant = new VideoGrant();
-    token.addGrant(grant);
-    const response = {
-      identity: token.identity,
-      token: token.toJwt()
+  requestVideoChatAccess:function(){
+    if (Meteor.isServer){
+      const token = new AccessToken(
+        Meteor.settings.private.twilio.accountSid,
+        Meteor.settings.private.twilio.apiKey,
+        Meteor.settings.private.twilio.apiKeySecret
+      );
+      token.identity = Random.id();
+      const grant = new VideoGrant();
+      token.addGrant(grant);
+      const response = {
+        identity: token.identity,
+        token: token.toJwt()
+      }
+      return response;
     }
-    return response;
   }
-});
-
-
-
-//fix line
-app.get('/token', function(request, response) {
-  var identity = randomName();
-
-  // Create an access token which we will sign and return to the client,
-  // containing the grant we just created.
-  var token = new AccessToken(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_API_KEY,
-    process.env.TWILIO_API_SECRET
-  );
-
-  // Assign the generated identity to the token.
-  token.identity = identity;
-
-  // Grant the access token Twilio Video capabilities.
-  var grant = new VideoGrant();
-  token.addGrant(grant);
-
-  // Serialize the token to a JWT string and include it in a JSON response.
-  response.send({
-    identity: identity,
-    token: token.toJwt()
-  });
 });
